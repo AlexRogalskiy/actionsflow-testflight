c9b3c55ec7271dd6061f857332048c6c6bdd038c
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-07-06 13:30:13 +0300

    Added info on workflows
    Updates on github-actions

--- workflows/quotes.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function ({helpers}) {
-    // const result = await helpers.axios.get(
-    //     "https://styled-quotes.vercel.app/api?theme=buefy&layout=default"
-    // );
-
-    const message = "https://styled-quotes.vercel.app/api?theme=buefy&layout=default";
-
-    return {message: message};
-};
